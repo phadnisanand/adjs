b8d84741abd5aec6c18f2fa53fc8b5c39328622d
Apple <apple@Apples-MacBook-Pro.local>
2024-07-22 14:56:45 +0530

    error handnlig js

---
+++ js/error_handling.js
@@ -0,0 +1,34 @@
+// error handling javascript
+/*try {
+  let a = 10;
+  console.log(a);
+  if(a == 10) {
+    throw new Error('a value is 10');  // throw custom error handle by catch
+  }
+
+} catch(error) {
+	console.log(error.message);
+  console.log(error);
+
+
+} finally {
+  console.log('completed');
+}*/
+
+//"use strict";
+
+
+//breakpoints
+let a = 10;
+let b = 20;
+
+function abc(a,b) { 
+  console.log(a);
+  b = 5;
+
+  console.log(a+ b);
+  //debugger;  use debugger keyword
+}
+abc(a,b);
+console.log('coming here');
+console.error('coming here');
